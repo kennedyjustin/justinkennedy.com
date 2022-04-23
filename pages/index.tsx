0ee773ff5f96054ceb28b65e9afecc3934c2cd4a
Justin Kennedy <iamjustinkennedy@gmail.com>
2022-04-23 16:34:25 -0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,48 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { MailIcon } from '@heroicons/react/solid'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>justinkennedy.com</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          Justin Kennedy
+        </h1>
+
+        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+          <a
+            href="https://vendrive.com"
+            target='_blank'
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">CTO @ Vendrive &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Unifying Brands &amp; Retailers.
+            </p>
+          </a>
+        </div>
+
+        <div className='mt-8 flex max-w-xs flex-wrap items-center justify-around sm:w-full'>
+          <a href='mailto:justin@justinkennedy.com' target='_blank'>
+            <MailIcon className="h-8 w-8 text-black"/>
+          </a>
+          <a href='https://github.com/kennedyjustin' target='_blank'>
+            <Image src="/github.svg" alt="Github" width={32} height={32} />
+          </a>
+          <a href='https://linkedin.com/in/justinkennedy10' target='_blank'>
+            <Image src="/linkedin.svg" alt="Linkedin" width={32} height={32} />
+          </a>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Home
