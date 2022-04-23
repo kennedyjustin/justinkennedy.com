dd57d0eabae5454492f1f0121ef81c04432b057b
Justin Kennedy <iamjustinkennedy@gmail.com>
2022-04-23 16:41:02 -0400

    remove blue hover

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPage = () => {
           <a
             href="https://vendrive.com"
             target='_blank'
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+            className="mt-6 w-96 rounded-xl border p-6 text-left"
           >
             <h3 className="text-2xl font-bold">CTO @ Vendrive &rarr;</h3>
             <p className="mt-4 text-xl">
