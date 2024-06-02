0304e739085a2bec4165aa2acc11807d8c116f08
Justin Kennedy <justin@justinkennedy.com>
2024-06-02 16:19:42 -0400

    add aws rewrite

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites() {
+        return {
+            beforeFiles: [
+                {
+                    source: '/:path*',
+                    has: [
+                        {
+                            type: 'host',
+                            value: 'aws.justinkennedy.com',
+                        },
+                    ],
+                    destination: 'https://justinkennedy.awsapps.com/start',
+                },
+            ]
+        }
+    },
+}
 
 module.exports = nextConfig
